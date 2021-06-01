fb18d59ba5c8e9a30d0237d1eb8ab52ede9872b6
ForLoveOfCats <floc@unpromptedtirade.com>
2021-06-01 00:11:10 -0400

    Move markdown processing into separate function

--- src/main.rs
+++ src/main.rs
@@ -74,6 +74,118 @@ struct Buffers {
 	author: String,
 }
 
+fn process_markdown(_fragments: &Fragments, args: &Arguments, buffers: &mut Buffers) {
+	let mut options = Options::empty();
+	options.insert(Options::ENABLE_TABLES);
+	let parser = Parser::new_ext(&buffers.input, options);
+
+	/*
+	 * NOTE: Borrowing these here borrows just the field instead of the entire
+	 * struct which allows the closure to have mutable access to these two fields
+	 * while `html::push_html` writes to another field.
+	 */
+	let title_buffer = &mut buffers.title;
+	title_buffer.clear();
+	let description_buffer = &mut buffers.description;
+	description_buffer.clear();
+	let author_buffer = &mut buffers.author;
+	author_buffer.clear();
+
+	let parser = parser.map(|event| {
+		if let Event::Html(html) = &event {
+			let html = html.trim();
+			if html.starts_with("<!--") && html.ends_with("-->") {
+				//We are reasonably confident that this is an HTML comment
+
+				let contents = &html["<!--".len()..];
+				let contents = &contents[..contents.len() - "-->".len()];
+
+				if let Some(colon_index) = contents.find(':') {
+					let label = &contents[..colon_index];
+					let trailing = contents[colon_index + 1..].trim();
+
+					match label {
+						"title" => {
+							title_buffer.clear();
+							title_buffer.push_str(trailing);
+						}
+
+						"description" => {
+							description_buffer.clear();
+							description_buffer.push_str(trailing);
+						}
+
+						"author" => {
+							author_buffer.clear();
+							author_buffer.push_str(trailing);
+						}
+
+						_ => {}
+					}
+				}
+			}
+		}
+		event
+	});
+
+	buffers.html.clear();
+	html::push_html(&mut buffers.html, parser);
+
+	buffers.output.clear();
+	buffers.output.push_str("<!DOCTYPE html>\n");
+	if let Some(language) = &args.language {
+		let _ = writeln!(buffers.output, r#"<html lang="{}">"#, language);
+	}
+	buffers.output.push_str(multiline!(
+		"<head>"
+		r#"<meta charset="UTF-8">"#
+	));
+	if !buffers.title.is_empty() {
+		let _ = writeln!(buffers.output, "<title>{}</title>", buffers.title);
+	}
+	if let Some(favicon) = &args.favicon {
+		let _ = writeln!(
+			buffers.output,
+			r#"<link rel="shortcut icon" type="image/png" href="{}" />"#,
+			favicon
+		);
+	}
+	if !buffers.description.is_empty() {
+		let _ = write!(
+			buffers.output,
+			multiline!(
+				r#"<meta name="description" content="{description}" />"#
+				r#"<meta property="og:description" content="{description}" />"#
+			),
+			description = buffers.description,
+		);
+	}
+	if !buffers.author.is_empty() {
+		let _ = writeln!(
+			buffers.output,
+			r#"<meta name="author" content="{}" />"#,
+			buffers.author
+		);
+	}
+	if let Some(opengraph_locale) = &args.opengraph_locale {
+		let _ = writeln!(
+			buffers.output,
+			r#"<meta property="og:locale" content="{}" />"#,
+			opengraph_locale
+		);
+	}
+	if let Some(opengraph_sitename) = &args.opengraph_sitename {
+		let _ = writeln!(
+			buffers.output,
+			r#"<meta property="og:site_name" content="{}" />"#,
+			opengraph_sitename
+		);
+	}
+	buffers.output.push_str("</head>\n");
+
+	buffers.output.push_str(&buffers.html);
+}
+
 fn process_file(
 	path: PathBuf,
 	output_path: PathBuf,
@@ -128,115 +240,7 @@ fn process_file(
 			std::process::exit(-1);
 		}
 
-		let mut options = Options::empty();
-		options.insert(Options::ENABLE_TABLES);
-		let parser = Parser::new_ext(&buffers.input, options);
-
-		/*
-		 * NOTE: Borrowing these here borrows just the field instead of the entire
-		 * struct which allows the closure to have mutable access to these two fields
-		 * while `html::push_html` writes to another field.
-		 */
-		let title_buffer = &mut buffers.title;
-		title_buffer.clear();
-		let description_buffer = &mut buffers.description;
-		description_buffer.clear();
-		let author_buffer = &mut buffers.author;
-		author_buffer.clear();
-
-		let parser = parser.map(|event| {
-			if let Event::Html(html) = &event {
-				let html = html.trim();
-				if html.starts_with("<!--") && html.ends_with("-->") {
-					//We are reasonably confident that this is an HTML comment
-
-					let contents = &html["<!--".len()..];
-					let contents = &contents[..contents.len() - "-->".len()];
-
-					if let Some(colon_index) = contents.find(':') {
-						let label = &contents[..colon_index];
-						let trailing = contents[colon_index + 1..].trim();
-
-						match label {
-							"title" => {
-								title_buffer.clear();
-								title_buffer.push_str(trailing);
-							}
-
-							"description" => {
-								description_buffer.clear();
-								description_buffer.push_str(trailing);
-							}
-
-							"author" => {
-								author_buffer.clear();
-								author_buffer.push_str(trailing);
-							}
-
-							_ => {}
-						}
-					}
-				}
-			}
-			event
-		});
-
-		buffers.html.clear();
-		html::push_html(&mut buffers.html, parser);
-
-		buffers.output.clear();
-		buffers.output.push_str("<!DOCTYPE html>\n");
-		if let Some(language) = &args.language {
-			let _ = writeln!(buffers.output, r#"<html lang="{}">"#, language);
-		}
-		buffers.output.push_str(multiline!(
-			"<head>"
-			r#"<meta charset="UTF-8">"#
-		));
-		if !buffers.title.is_empty() {
-			let _ = writeln!(buffers.output, "<title>{}</title>", buffers.title);
-		}
-		if let Some(favicon) = &args.favicon {
-			let _ = writeln!(
-				buffers.output,
-				r#"<link rel="shortcut icon" type="image/png" href="{}" />"#,
-				favicon
-			);
-		}
-		if !buffers.description.is_empty() {
-			let _ = write!(
-				buffers.output,
-				multiline!(
-					r#"<meta name="description" content="{description}" />"#
-					r#"<meta property="og:description" content="{description}" />"#
-				),
-				description = buffers.description,
-			);
-		}
-		if !buffers.author.is_empty() {
-			let _ = writeln!(
-				buffers.output,
-				r#"<meta name="author" content="{}" />"#,
-				buffers.author
-			);
-		}
-		if let Some(opengraph_locale) = &args.opengraph_locale {
-			let _ = writeln!(
-				buffers.output,
-				r#"<meta property="og:locale" content="{}" />"#,
-				opengraph_locale
-			);
-		}
-		if let Some(opengraph_sitename) = &args.opengraph_sitename {
-			let _ = writeln!(
-				buffers.output,
-				r#"<meta property="og:site_name" content="{}" />"#,
-				opengraph_sitename
-			);
-		}
-		buffers.output.push_str("</head>\n");
-
-		buffers.output.push_str(&buffers.html);
+		process_markdown(_fragments, args, buffers);
 
 		if let Err(err) = std::fs::write(&output_path, &buffers.output) {
 			eprintln!(
